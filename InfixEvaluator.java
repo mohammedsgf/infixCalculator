a313fbe1446a7eab858345cd84f101740bdf7cf7
Mohammed <Mohammedsgf@gmail.com>
2020-03-03 18:22:45 +0300

    Full Program, still needs to be documted and add some comments

--- InfixEvaluator.java
+++ InfixEvaluator.java
@@ -5,134 +5,77 @@ public class InfixEvaluator {
 	private static ArrayStack<Double> evaluter=new ArrayStack<Double>();
 	private static StringBuilder postfixExp=new StringBuilder();
 	
-	private static char chrs;
+	private static Character chrs;
 	public static void main(String[] args) {
 		
 		
 		Scanner input=new Scanner(System.in);
 		System.out.print("Write Your expression : ");
 		String inExp=input.nextLine().replaceAll(" ","");
+		input.close();
 		
 		
-		//Scanner read=new Scanner(inExp);
-		 //double total;
 		String digits="";
+		
 		for (int i = 0; i < inExp.length(); i++) {
              chrs = inExp.charAt(i);     
             
-            if (Character.isDigit(chrs)||chrs=='.') {//if char is digit or a dot
+            if (Character.isDigit(chrs) || chrs=='.') {//if char is digit or a dot
                 digits = digits+chrs; //add the char to the digit
+                
                  if(i==inExp.length()-1) { //if i is in the last index 
+                	 
                 	evaluter.push(Double.parseDouble(digits)); //push the latest value of the digit
                 	postfixExp.append(digits);//Build the postfix expression
                 	digits="";
+                	
                 	while(!opStack.isEmpty()) {
-                		
                 		int oldOP=decodeOp(opStack.peek());
-                		
-                			if(oldOP==3) {
-                				postfixExp.append(opStack.pop());
-                				double firstValue=evaluter.pop();
-                				double secondValue=evaluter.pop();
-                				evaluate(firstValue,secondValue,"^");
-                				
-                			}
-                			else if(oldOP==2)
-                				if(opStack.peek().equals("*")) {
-                					postfixExp.append(opStack.pop());
-                    				double firstValue=evaluter.pop();
-                    				double secondValue=evaluter.pop();
-                    				evaluate(firstValue,secondValue,"*");
-                    				
-                				}
-                				else {
-                					postfixExp.append(opStack.pop());
-                    				double firstValue=evaluter.pop();
-                    				double secondValue=evaluter.pop();
-                    				evaluate(firstValue,secondValue,"/");
-                    				
-                				}
-                			else if(oldOP==1) {
-                				if(opStack.peek().equals("+")) {
-                					postfixExp.append(opStack.pop());
-                    				double firstValue=evaluter.pop();
-                    				double secondValue=evaluter.pop();
-                    				evaluate(firstValue,secondValue,"+");
-                    				
-                				}
-                				else {
-                					postfixExp.append(opStack.pop());
-                    				double firstValue=evaluter.pop();
-                    				double secondValue=evaluter.pop();
-                    				evaluate(firstValue,secondValue,"-");
-                    				
-                				}
-                			}
-                		}
-                		
-                	}
+                		prefixEvaluator(oldOP);
+                	}	
                 }
-            
+            }
             else if(!Character.isDigit(chrs)){ //if the char is not a digit
+            	
+            	if(!digits.isEmpty()) {
+            		
             	evaluter.push(Double.parseDouble(digits));//push the digit
             	postfixExp.append(digits);//Build the postfix expression
             	digits=""; //clear the digit
-           //----------------------------------------------------------
             	
-            	if(opStack.isEmpty()) {
-            	opStack.push(""+chrs);// Must add more details about the input
+            	}
+//----------------------------------------------------------
+            	if(chrs.equals('(')) {
+            		//do nothing
+            	}
+            	else if(chrs.equals(')')) {
+            		
+            		int oldOP=decodeOp(opStack.peek());
+            		prefixEvaluator(oldOP);
+            		
+            		if(i==inExp.length()-1) { //if i is in the last index 
+                    	
+            			while(!opStack.isEmpty()) {
+                    		int tempOp=decodeOp(opStack.peek());
+                    		prefixEvaluator(tempOp);
+                    	}	
+                    }
+            	}
+            	else if(opStack.isEmpty()) {
+            		opStack.push(""+chrs);// Must add more details about the input
             	}
             	else {
             		int newOP=decodeOp(""+chrs);
             		int oldOP=decodeOp(opStack.peek());
             		if(newOP>oldOP) {
             			opStack.push(""+chrs);
-            			postfixExp.append(chrs);
             		}
             		else {
-            			if(oldOP==3) {
-            				postfixExp.append(opStack.pop());
-            				double firstValue=evaluter.pop();
-            				double secondValue=evaluter.pop();
-            				evaluate(firstValue,secondValue,"^");
-            				opStack.push(""+chrs);
-            			}
-            			else if(oldOP==2)
-            				if(opStack.peek().equals("*")) {
-            					postfixExp.append(opStack.pop());
-                				double firstValue=evaluter.pop();
-                				double secondValue=evaluter.pop();
-                				evaluate(firstValue,secondValue,"*");
-                				opStack.push(""+chrs);
-            				}
-            				else {
-            					postfixExp.append(opStack.pop());
-                				double firstValue=evaluter.pop();
-                				double secondValue=evaluter.pop();
-                				evaluate(firstValue,secondValue,"/");
-                				opStack.push(""+chrs);
-            				}
-            			else if(oldOP==1) {
-            				if(opStack.peek().equals("+")) {
-            					postfixExp.append(opStack.pop());
-                				double firstValue=evaluter.pop();
-                				double secondValue=evaluter.pop();
-                				evaluate(firstValue,secondValue,"+");
-                				opStack.push(""+chrs);
-            				}
-            				else {
-            					postfixExp.append(opStack.pop());
-                				double firstValue=evaluter.pop();
-                				double secondValue=evaluter.pop();
-                				evaluate(firstValue,secondValue,"-");
-                				opStack.push(""+chrs);
-            				}
-            			}
+            			prefixEvaluator(oldOP);
+            			opStack.push(""+chrs);
             		}
             	}
-            }
-            
-            
+            }  
         }
 		
 		
@@ -143,6 +86,9 @@ public class InfixEvaluator {
 		System.out.println("Pop " +evaluter.pop());
 		}
 	}
+	
+	
+	
 	public static int  decodeOp(String op) {
 		if(op.equals("^")) {
 			return 3;
@@ -185,6 +131,42 @@ public class InfixEvaluator {
 			evaluter.push(calc);
 		}
 		
+	}
+	
+	public static void prefixEvaluator(int oldOP) {
+		if(oldOP==3) {
+			postfixExp.append(opStack.pop());
+			double firstValue=evaluter.pop();
+			double secondValue=evaluter.pop();
+			evaluate(firstValue,secondValue,"^");
+		}
+		else if(oldOP==2)
+			if(opStack.peek().equals("*")) {
+				postfixExp.append(opStack.pop());
+				double firstValue=evaluter.pop();
+				double secondValue=evaluter.pop();
+				evaluate(firstValue,secondValue,"*");
+			}
+			else {
+				postfixExp.append(opStack.pop());
+				double firstValue=evaluter.pop();
+				double secondValue=evaluter.pop();
+				evaluate(firstValue,secondValue,"/");
+			}
+		else if(oldOP==1) {
+			if(opStack.peek().equals("+")) {
+				postfixExp.append(opStack.pop());
+				double firstValue=evaluter.pop();
+				double secondValue=evaluter.pop();
+				evaluate(firstValue,secondValue,"+");
+			}
+			else {
+				postfixExp.append(opStack.pop());
+				double firstValue=evaluter.pop();
+				double secondValue=evaluter.pop();
+				evaluate(firstValue,secondValue,"-");
+			}
+		}
 	}
 
 }
